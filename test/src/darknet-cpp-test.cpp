8558e36c80f5b2c888ccbff58d97b7a025ea2692
Pierre-Jean Clement <pj.clement@two-i.fr>
2018-10-22 15:17:04 +0100

    Update test to return non zero exit codes on failure

--- test/src/darknet-cpp-test.cpp
+++ test/src/darknet-cpp-test.cpp
@@ -1,42 +1,56 @@
 #include <yolopp.h>
 #include <opencv2/opencv.hpp>
-int main()
+using namespace std;
+
+int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        cerr << "Expecting exactly 1 argument: path to the directory containing the config files";
+        return 1;
+    }
+
+    // get data dir path from the command line arguments
+    // add a slash at the end if it's not present
+    string path = argv[1];
+    if (path[path.length()-1] != '/')
+        path = path + '/';
+
+    // files used to configure the network
+    string cfg = path + "yolov3-tiny.cfg";
+    string weights = path + "yolov3-tiny.weights";
+
+    // images we want to test
+    string imgfile = path + "dog.jpg";
+    
+    // ouptut for the command line
+    cout << endl;
+    cout << "Using the following files to configure the network:" << endl;
+    cout << " - " << data << endl;
+    cout << " - " << cfg << endl;
+    cout << " - " << weights << endl;
+    cout << endl;
+    cout << "Using the following files to test the network:" << endl;
+    cout << " - " << data << endl;
+    cout << endl;
+
     Yolopp y;
-
-    // std::string path = "/home/charlie/Documents/Two-i/mono-runtime/yoloCFG/V3-416/";
-    // std::string cfg = path + "input.cfg";
-    // std::string data = path + "input.data";
-    // std::string w = path + "input.weights";
-    // y.load(data, cfg, w);
-    // cv::Mat img = cv::imread("/home/charlie/Documents/Two-i/darknet_pj/arapaho/darknetcpplogo.png");
-
-    // std::string path = "/home/pj/tmp/darknet-wrksx/";
-    // std::string cfg = path + "cfg/yolov3.cfg";
-    // std::string data = path + "cfg/coco.data";
-    // std::string w = path + "data/yolov3.weights";
-    // paths relative to the current directory
-    std::string data = "data/coco.data";
-    std::string cfg = "data/yolov3.cfg";
-    std::string w = "data/yolov3.weights";
-    y.load(data, cfg, w);
-
-    // the images we want to test
-    std::string test_images[4] = {
-        "data/dog.jpg",
-        "data/eagle.jpg",
-        "data/giraffe.jpg",
-        "data/horses.jpg",
-    };
-
-        // Print Strings
-    for (int i = 0; i < 4; i++) {
-        cv::Mat img = cv::imread(test_images[i]);
-        std::vector<yoloDetections> v = y.detect(img, 0.45, 0.5);
-        for(yoloDetections det : v) {
-            std::cout << det.label << " ";
-        }
-        std::cout << std::endl;
+    bool loaded = y.load(data, cfg, weights);
+    if (!loaded) 
+        return 2;
+    
+    try 
+    {
+        cv::Mat img = cv::imread(imgfile);
+        vector<yoloDetections> v = y.detect(img, 0.45, 0.5);
+        for (yoloDetections det : v)
+            cout << det.label << " ";
+        
+        cout << endl;
+    }
+    catch (string error)
+    {
+        return 3;
     }
 
     return 0;
