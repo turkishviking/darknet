515bddd2efe5b23df8066a6cd41539b202391080
Pierre-Jean Clement <pj.cleme@gmail.com>
2018-10-22 09:55:37 +0200

    Update the test to use files from the project in the test/test_data/ dir, and copy these test at configure time so test can run

--- test/src/darknet-cpp-test.cpp
+++ test/src/darknet-cpp-test.cpp
@@ -11,15 +11,33 @@ int main()
     // y.load(data, cfg, w);
     // cv::Mat img = cv::imread("/home/charlie/Documents/Two-i/darknet_pj/arapaho/darknetcpplogo.png");
 
-    std::string path = "/home/pj/tmp/darknet-wrksx/";
-    std::string cfg = path + "cfg/yolov3.cfg";
-    std::string data = path + "cfg/coco.data";
-    std::string w = path + "data/yolov3.weights";
+    // std::string path = "/home/pj/tmp/darknet-wrksx/";
+    // std::string cfg = path + "cfg/yolov3.cfg";
+    // std::string data = path + "cfg/coco.data";
+    // std::string w = path + "data/yolov3.weights";
+    // paths relative to the current directory
+    std::string data = "data/coco.data";
+    std::string cfg = "data/yolov3.cfg";
+    std::string w = "data/yolov3.weights";
     y.load(data, cfg, w);
-    cv::Mat img = cv::imread("/home/pj/tmp/darknet-wrksx/arapaho/darknetcpplogo.png");
 
-    std::vector<yoloDetections> v = y.detect(img, 0.45, 0.5);
-    for(yoloDetections det : v)
-        std::cout << det.label << std::endl;
+    // the images we want to test
+    std::string test_images[4] = {
+        "data/dog.jpg",
+        "data/eagle.jpg",
+        "data/giraffe.jpg",
+        "data/horses.jpg",
+    };
+
+        // Print Strings
+    for (int i = 0; i < 4; i++) {
+        cv::Mat img = cv::imread(test_images[i]);
+        std::vector<yoloDetections> v = y.detect(img, 0.45, 0.5);
+        for(yoloDetections det : v) {
+            std::cout << det.label << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
