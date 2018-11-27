dfa409bcbe421f34ea51ee81820c7634d0d067bd
Charlie <c.lutaud@hotmail.fr>
2018-11-27 16:28:28 +0100

    add .o (for fucking cuda mes couille)

--- conanfile.py
+++ conanfile.py
@@ -61,6 +61,7 @@ class DarknetcppConan(ConanFile):
         self.copy('*.so', dst='lib', keep_path=False)
         self.copy('*.dylib', dst='lib', keep_path=False)
         self.copy('*.a', dst='lib', keep_path=False)
+        self.copy('*.o', dst='lib', keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ['darknet-cpp']
